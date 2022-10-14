9d4c8ce0ee5b6c20c9d9f6689356bcbf6c40cae1
hiuchida <hiro4uchida@gmail.com>
2022-10-14 23:31:23 +0900

    Sync StdoutLog.timethreadprintln

--- src/main/java/util/StdoutLog.java
+++ src/main/java/util/StdoutLog.java
@@ -53,4 +53,22 @@ public class StdoutLog {
 		System.out.println(now + " " + name + "." + method + ": " + msg);
 	}
 
+	/**
+	 * タイムスタンプとスレッドIDをつけて、ログ出力する（改行あり）。
+	 * 
+	 * @param clazz  実行クラス。
+	 * @param method メソッド名。
+	 * @param msg    メッセージ文字列。
+	 */
+	public static void timethreadprintln(Class<?> clazz, String method, String msg) {
+		String now = DateTimeUtil.nowToString();
+		long threadid = Thread.currentThread().getId();
+		String name = clazz.getName();
+		int idx = name.lastIndexOf(".");
+		if (idx >= 0) {
+			name = name.substring(idx + 1);
+		}
+		System.out.println(now + " [" + threadid + "] " + name + "." + method + ": " + msg);
+	}
+
 }
