f19989f97e77e53d43d9df5cfc7f2632550b5547
hiuchida <hiro4uchida@gmail.com>
2022-10-16 08:48:05 +0900

    デプロイ可能なwarファイルを作成する

--- src/main/java/server/SampleTomcatApplication.java
+++ src/main/java/server/SampleTomcatApplication.java
@@ -23,15 +23,22 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SampleTomcatApplication {
+public class SampleTomcatApplication extends SpringBootServletInitializer {
 
 	private static Log logger = LogFactory.getLog(SampleTomcatApplication.class);
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SampleTomcatApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SampleTomcatApplication.class);
 	}
 
 	@Bean
